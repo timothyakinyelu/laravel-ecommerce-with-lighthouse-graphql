bbd6d24fef2bdaede22e9072719430a2fd894872
loverofMush <tolujamal1@gmail.com>
2020-06-25 01:37:54 +0100

    class categoriestest created

---
+++ tests/Feature/Category/CategoriesTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Tests\Feature\Category;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+use App\Category;
+
+class CategoriesTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * A basic feature test example.
+     *
+     * @test
+     */
+    public function can_get_name_from_categories(): void
+    {
+        $category1 = factory(Category::class)->create(['name' => "Cat1"]);
+        $category2 = factory(Category::class)->create(['name' => "Cat2"]);
+        $category3 = factory(Category::class)->create(['name' => "Cat3"]);
+
+        $response = $this->graphQL(/** @lang GraphQL */ '
+            {
+                categories {
+                    id
+                    name
+                }
+            }
+        ');
+
+        $names = $response->json("data.categories.*.name");
+
+        $this->assertCount(3, $names);
+
+        $this->assertSame(
+            [
+                "Cat1",
+                "Cat2",
+                "Cat3"
+            ], $names
+        );
+    }
+}
