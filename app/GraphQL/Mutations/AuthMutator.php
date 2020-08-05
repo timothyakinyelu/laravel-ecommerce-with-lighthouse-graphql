1f4cb34c8f10577e7dd1b09cec491f1279d08b60
loverofMush <tolujamal1@gmail.com>
2020-08-05 05:36:32 +0100

    - AuthMutator for testing - Update still required

--- app/GraphQL/Mutations/AuthMutator.php
+++ app/GraphQL/Mutations/AuthMutator.php
@@ -4,6 +4,15 @@ namespace App\GraphQL\Mutations;
 
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Hash;
+use Nuwave\Lighthouse\Exceptions\AuthenticationException;
+use Nuwave\Lighthouse\Support\Contracts\GraphQLContext;
+use Illuminate\Support\Facades\Cookie;
+use Illuminate\Support\Str;
+
+use App\User;
+use App\Role;
 
 class AuthMutator
 {
@@ -11,19 +20,29 @@ class AuthMutator
      * @param  null  $_
      * @param  array<string, mixed>  $args
      */
-    public function resolve($_, array $args)
+    public function resolve($_, array $args, GraphQLContext $context = null)
     {
         $credentials = Arr::only($args, ['email', 'password']);
 
-        if (Auth::once($credentials)) {
-
-            $user = auth()->user();
-            // return $user;
-            // return $user->hasRole('developer');
-            // return $user->givePermissionsTo('edit-tasks');
-            return $user->can('edit-tasks');
+        if(!Auth::once($credentials)) {
+            throw new AuthenticationException('Login Failed');
         }
 
-        return null;
+        if(!$context->request->hasCookie('_token')) {
+            $token = Auth::user()->createToken('access token')->accessToken;
+            Cookie::queue('_token', $token, 1800, '/', $context->request->getHost(), false, true);
+        }
+        return $token;
+
+        // if (Auth::once($credentials)) {
+
+        //     $user = auth()->user();
+        //     // return $user;
+        //     // return $user->hasRole('developer');
+        //     // return $user->givePermissionsTo('edit-tasks');
+        //     return $user->can('edit-tasks');
+        // }
+
+        // return null;
     }
 }
