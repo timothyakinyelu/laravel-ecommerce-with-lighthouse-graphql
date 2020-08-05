3094aebc809f39f73129f8379738123764cde811
loverofMush <tolujamal1@gmail.com>
2020-08-05 05:46:26 +0100

    - use League\OAuth2\Server\AuthorizationServer - use Laravel\Passport\Bridge\RefreshTokenRepository - enableGrantType with access_token - setRefreshTokenTTL with refresh_token

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,7 +3,13 @@
 namespace App\Providers;
 
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use League\OAuth2\Server\AuthorizationServer;
+use Laravel\Passport\Bridge\RefreshTokenRepository;
+// use League\OAuth2\Server\Repositories\RefreshTokenRepositoryInterface;
+use App\CustomGrants\VerifyEmailGrant;
 use Illuminate\Support\Facades\Gate;
+use Laravel\Passport\Passport;
+use Carbon\Carbon;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -25,6 +31,34 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
+        app(AuthorizationServer::class)->enableGrantType(
+            $this->makeVerifyEmailGrant(), Passport::tokensExpireIn()
+        );
+
+
         //
+        Passport::routes();
+
+        Passport::personalAccessClientId(
+            config('passport.personal_access_client.id')
+        );
+    
+        Passport::personalAccessClientSecret(
+            config('passport.personal_access_client.secret')
+        );
+
+        // Passport::tokensExpireIn(Carbon::now()->addMinutes(10));
+        // Passport::refreshTokensExpireIn(Carbon::now()->addDays(10));
+    }
+
+    protected function makeVerifyEmailGrant()
+    {
+        $grant = new VerifyEmailGrant(
+            $this->app->make(RefreshTokenRepository::class)
+        );
+
+        $grant->setRefreshTokenTTL(Passport::refreshTokensExpireIn());
+
+        return $grant;
     }
 }
